9b5bc8ff16b79ed6652c1cd9366a70e7c30bc7a6
Wanda W <wanda.yanli@gmail.com>
2017-01-29 03:08:21 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,6 +4,17 @@ rows = 'ABCDEFGHI'
 cols = '123456789'
 possible_digits = '123456789'
 
+boxes = cross(rows, cols)
+row_units = [cross(row, cols) for row in rows]
+column_units = [cross(rows, col) for col in cols]
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+
+diagonal_units = [[row + cols[rows.index(row)] for row in rows],[row + cols[sorted(rows,reverse=True).index(row)] for row in rows]]
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((box, [unit for unit in unitlist if box in unit]) for box in boxes)
+peers = dict((box, set(sum(units[box], []))-set([box])) for box in boxes)
+
+
 def cross(a, b):
     return [s + t for s in a for t in b]
 
@@ -71,17 +82,6 @@ def display(values):
         print(''.join(values[row + col].center(width) + ('|' if col in '36' else '') for col in cols))
         if row in 'CF': print(line)
     return
-
-boxes = cross(rows, cols)
-row_units = [cross(row, cols) for row in rows]
-column_units = [cross(rows, col) for col in cols]
-square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-
-diagonal_units = [[row + cols[rows.index(row)] for row in rows],[row + cols[sorted(rows,reverse=True).index(row)] for row in rows]]
-unitlist = row_units + column_units + square_units + diagonal_units
-units = dict((box, [unit for unit in unitlist if box in unit]) for box in boxes)
-peers = dict((box, set(sum(units[box], []))-set([box])) for box in boxes)
-
 
 def return_pairs(sudoku):
     return [box for box in boxes if len(sudoku[box]) == 2]
