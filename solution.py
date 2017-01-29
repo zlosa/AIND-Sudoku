c1b3cc7577b2c9970388fe9ccbed4f9d57d6efb0
Wanda W <wanda.yanli@gmail.com>
2017-01-29 03:10:29 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,19 +4,17 @@ rows = 'ABCDEFGHI'
 cols = '123456789'
 possible_digits = '123456789'
 
+def cross(a, b):
+    return [s + t for s in a for t in b]
+
 boxes = cross(rows, cols)
 row_units = [cross(row, cols) for row in rows]
 column_units = [cross(rows, col) for col in cols]
 square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-
 diagonal_units = [[row + cols[rows.index(row)] for row in rows],[row + cols[sorted(rows,reverse=True).index(row)] for row in rows]]
 unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((box, [unit for unit in unitlist if box in unit]) for box in boxes)
 peers = dict((box, set(sum(units[box], []))-set([box])) for box in boxes)
-
-
-def cross(a, b):
-    return [s + t for s in a for t in b]
 
 def assign_value(sudoku, box, value):
     """Please use this function to update your values dictionary!
